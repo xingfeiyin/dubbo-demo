0602952a519c189c33c00dee6784e559cd284f27
yinxf <79241676@qq.com>
2021-05-22 18:37:55 +0800

    add

---
+++ dubbo-consumer/src/main/java/com/yinxf/consumer/controller/User.java
@@ -0,0 +1,13 @@
+package com.yinxf.consumer.controller;
+
+import lombok.Data;
+
+/**
+ * @author yinxf
+ * @date 2021/5/22
+ * @description
+ */
+@Data
+public class User {
+    private String userId;
+}
